f6cbe2a9237ee4d381f325844c34676aaaf28ad1
RaffaeleMarchisio <raffaele.marchisio@itiscuneo.eu>
2022-10-03 13:20:02 +0200

    aggiornato il codice

--- esercizi/client-server-UDP/client.py
+++ esercizi/client-server-UDP/client.py
@@ -1,17 +1,17 @@
 from socket import AF_INET,socket,SOCK_DGRAM,SOL_SOCKET,SO_BROADCAST
-
-def chat_Client(HOST,PORT):
+from packet import Packet
+def chat_Client(HOST,PORT,username):
     with socket(AF_INET,SOCK_DGRAM) as s:
         s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
         while True:
-            msg=input("messaggio")
-            msg= msg.encode('utf8')
-            s.sendto(msg,(HOST,PORT))
+            msg=input("inserire messaggio:")
+            p=Packet(username,msg)
+            s.sendto(p.to_bytes(),(HOST,PORT))
             print("messaggio inviato")
 
 
 if __name__ == '__main__':
+    username=input("inserire nome:")
     HOST=input("inserire l'indirizzo ip dell'host:")
-    PORT=int(input("inserire porta:"))
-    chat_Client(HOST, PORT)
-
+    PORT=int(input("inserire porta"))
+    chat_Client(HOST, PORT,username)
